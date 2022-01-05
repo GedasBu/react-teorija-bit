df423a93d2fd02746f71a21a3ae4f9fc4cbb6422
GedasBu <86909912+GedasBu@users.noreply.github.com>
2022-01-05 10:02:33 +0200

    final

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+const App = ()=>{
+
+    return 
+    (
+        <div className="App col top">
+        <div className="books">
+          <h1>Naujas projektas</h1>          
+         
+        </div>
+      </div>
+
+    )
+}
